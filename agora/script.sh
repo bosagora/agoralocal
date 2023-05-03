38f1afcda37c7af031ec944a9a272d979fc90e6e
Michael Kim <michael.kim.200518@gmail.com>
2023-05-03 09:43:09 +0900

    Use quote this to prevent word splitting in bash script

--- agora/script.sh
+++ agora/script.sh
@@ -25,73 +25,73 @@ fi
 
 if [ "$1" = "init" ]; then
 
-    npx ts-node $agora_root/adjustment/createForkData.ts
-    npx ts-node $agora_root/adjustment/replaceGenesisTimeStamp.ts $agora_root/config/cl/chain-config-template.yaml $agora_root/config/cl/chain-config.yaml
-    npx ts-node $agora_root/adjustment/replaceGenesisTimeStamp.ts $agora_root/config/cl/chain-config-template-capella.yaml $agora_root/config/cl/chain-config-capella.yaml
-    npx ts-node $agora_root/adjustment/replaceGenesisTimeStamp.ts $agora_root/config/el/genesis-template-shanghai.json $agora_root/config/el/genesis-shanghai.json
-    npx ts-node $agora_root/adjustment/replaceGenesisTimeStamp.ts $agora_root/config/el/genesis-template.json $agora_root/config/el/genesis.json
-    npx ts-node $agora_root/adjustment/replaceGenesisTimeStamp.ts $agora_root/config/scan/default.config-template.yml $agora_root/config/scan/default.config.yml
+    npx ts-node "$agora_root"/adjustment/createForkData.ts
+    npx ts-node "$agora_root"/adjustment/replaceGenesisTimeStamp.ts "$agora_root"/config/cl/chain-config-template.yaml "$agora_root"/config/cl/chain-config.yaml
+    npx ts-node "$agora_root"/adjustment/replaceGenesisTimeStamp.ts "$agora_root"/config/cl/chain-config-template-capella.yaml "$agora_root"/config/cl/chain-config-capella.yaml
+    npx ts-node "$agora_root"/adjustment/replaceGenesisTimeStamp.ts "$agora_root"/config/el/genesis-template-shanghai.json "$agora_root"/config/el/genesis-shanghai.json
+    npx ts-node "$agora_root"/adjustment/replaceGenesisTimeStamp.ts "$agora_root"/config/el/genesis-template.json "$agora_root"/config/el/genesis.json
+    npx ts-node "$agora_root"/adjustment/replaceGenesisTimeStamp.ts "$agora_root"/config/scan/default.config-template.yml "$agora_root"/config/scan/default.config.yml
 
-    rm -rf $agora_root/chain
-    mkdir -p $agora_root/chain
+    rm -rf "$agora_root"/chain
+    mkdir -p "$agora_root"/chain
 
-    mkdir -p $agora_root/chain/node1
-    mkdir -p $agora_root/chain/node1/el
-    mkdir -p $agora_root/chain/node1/cl
+    mkdir -p "$agora_root"/chain/node1
+    mkdir -p "$agora_root"/chain/node1/el
+    mkdir -p "$agora_root"/chain/node1/cl
 
-    mkdir -p $agora_root/chain/node2
-    mkdir -p $agora_root/chain/node2/el
-    mkdir -p $agora_root/chain/node2/cl
+    mkdir -p "$agora_root"/chain/node2
+    mkdir -p "$agora_root"/chain/node2/el
+    mkdir -p "$agora_root"/chain/node2/cl
 
-    mkdir -p $agora_root/chain/node3
-    mkdir -p $agora_root/chain/node3/el
-    mkdir -p $agora_root/chain/node3/cl
+    mkdir -p "$agora_root"/chain/node3
+    mkdir -p "$agora_root"/chain/node3/el
+    mkdir -p "$agora_root"/chain/node3/cl
 
-    mkdir -p $agora_root/chain/node4
-    mkdir -p $agora_root/chain/node4/el
-    mkdir -p $agora_root/chain/node4/cl
+    mkdir -p "$agora_root"/chain/node4
+    mkdir -p "$agora_root"/chain/node4/el
+    mkdir -p "$agora_root"/chain/node4/cl
 
-    mkdir -p $agora_root/chain/node5
-    mkdir -p $agora_root/chain/node5/el
-    mkdir -p $agora_root/chain/node5/cl
+    mkdir -p "$agora_root"/chain/node5
+    mkdir -p "$agora_root"/chain/node5/el
+    mkdir -p "$agora_root"/chain/node5/cl
 
-    cp -rf $agora_root/config/el/template/node1/* $agora_root/chain/node1/el
-    cp -rf $agora_root/config/el/template/node2/* $agora_root/chain/node2/el
-    cp -rf $agora_root/config/el/template/node3/* $agora_root/chain/node3/el
-    cp -rf $agora_root/config/el/template/node4/* $agora_root/chain/node4/el
+    cp -rf "$agora_root"/config/el/template/node1/* "$agora_root"/chain/node1/el
+    cp -rf "$agora_root"/config/el/template/node2/* "$agora_root"/chain/node2/el
+    cp -rf "$agora_root"/config/el/template/node3/* "$agora_root"/chain/node3/el
+    cp -rf "$agora_root"/config/el/template/node4/* "$agora_root"/chain/node4/el
 
-    docker run -it -v $agora_root/chain/node1/el:/data -v $agora_root/config/el:/config --name el-node --rm bosagora/agora-el-node:v1.0.2 --datadir=/data init /config/genesis.json
-    docker run -it -v $agora_root/chain/node2/el:/data -v $agora_root/config/el:/config --name el-node --rm bosagora/agora-el-node:v1.0.2 --datadir=/data init /config/genesis.json
-    docker run -it -v $agora_root/chain/node3/el:/data -v $agora_root/config/el:/config --name el-node --rm bosagora/agora-el-node:v1.0.2 --datadir=/data init /config/genesis.json
-    docker run -it -v $agora_root/chain/node4/el:/data -v $agora_root/config/el:/config --name el-node --rm bosagora/agora-el-node:v1.0.2 --datadir=/data init /config/genesis.json
-    docker run -it -v $agora_root/chain/node5/el:/data -v $agora_root/config/el:/config --name el-node --rm bosagora/agora-el-node:v1.0.2 --datadir=/data init /config/genesis.json
+    docker run -it -v "$agora_root"/chain/node1/el:/data -v "$agora_root"/config/el:/config --name el-node --rm bosagora/agora-el-node:v1.0.2 --datadir=/data init /config/genesis.json
+    docker run -it -v "$agora_root"/chain/node2/el:/data -v "$agora_root"/config/el:/config --name el-node --rm bosagora/agora-el-node:v1.0.2 --datadir=/data init /config/genesis.json
+    docker run -it -v "$agora_root"/chain/node3/el:/data -v "$agora_root"/config/el:/config --name el-node --rm bosagora/agora-el-node:v1.0.2 --datadir=/data init /config/genesis.json
+    docker run -it -v "$agora_root"/chain/node4/el:/data -v "$agora_root"/config/el:/config --name el-node --rm bosagora/agora-el-node:v1.0.2 --datadir=/data init /config/genesis.json
+    docker run -it -v "$agora_root"/chain/node5/el:/data -v "$agora_root"/config/el:/config --name el-node --rm bosagora/agora-el-node:v1.0.2 --datadir=/data init /config/genesis.json
 
-    cp -f $agora_root/config/el/nodekey/node1.key $agora_root/chain/node1/el/geth/nodekey
-    cp -f $agora_root/config/el/nodekey/node2.key $agora_root/chain/node2/el/geth/nodekey
-    cp -f $agora_root/config/el/nodekey/node3.key $agora_root/chain/node3/el/geth/nodekey
-    cp -f $agora_root/config/el/nodekey/node4.key $agora_root/chain/node4/el/geth/nodekey
+    cp -f "$agora_root"/config/el/nodekey/node1.key "$agora_root"/chain/node1/el/geth/nodekey
+    cp -f "$agora_root"/config/el/nodekey/node2.key "$agora_root"/chain/node2/el/geth/nodekey
+    cp -f "$agora_root"/config/el/nodekey/node3.key "$agora_root"/chain/node3/el/geth/nodekey
+    cp -f "$agora_root"/config/el/nodekey/node4.key "$agora_root"/chain/node4/el/geth/nodekey
 
 elif [ "$1" = "start" ]; then
 
-    docker-compose -f $agora_root/nodes/docker-compose.yml up -d
+    docker-compose -f "$agora_root"/nodes/docker-compose.yml up -d
 
 elif [ "$1" = "stop" ]; then
 
-    docker-compose -f $agora_root/nodes/docker-compose.yml down
+    docker-compose -f "$agora_root"/nodes/docker-compose.yml down
 
     if [ -f "$agora_root/nodes-simulation/docker-compose.yml" ]; then
-        docker-compose -f $agora_root/nodes-simulation/docker-compose.yml down
+        docker-compose -f "$agora_root"/nodes-simulation/docker-compose.yml down
     fi
 
 elif [ "$1" = "attach" ]; then
 
-    docker run -it -v $agora_root/chain/node1/el:/data -v $agora_root/config/el:/config --name el-node-attach --rm bosagora/agora-el-node:v1.0.2 --datadir=/data attach /data/geth.ipc
+    docker run -it -v "$agora_root"/chain/node1/el:/data -v "$agora_root"/config/el:/config --name el-node-attach --rm bosagora/agora-el-node:v1.0.2 --datadir=/data attach /data/geth.ipc
 
 elif [ "$1" = "import" ]; then
 
     docker run -it \
-    -v $agora_root/config/cl:/config \
-    -v $agora_root/wallet:/wallet \
+    -v "$agora_root"/config/cl:/config \
+    -v "$agora_root"/wallet:/wallet \
     --name cl-validator --rm \
     bosagora/agora-cl-validator:v1.0.3 \
     accounts import \
@@ -102,24 +102,24 @@ elif [ "$1" = "import" ]; then
 
 elif [ "$1" = "start-db" ]; then
 
-    docker-compose -f $agora_root/postgres/docker-compose.yml up -d
+    docker-compose -f "$agora_root"/postgres/docker-compose.yml up -d
 
 elif [ "$1" = "stop-db" ]; then
 
-    docker-compose -f $agora_root/postgres/docker-compose.yml down
+    docker-compose -f "$agora_root"/postgres/docker-compose.yml down
 
 elif [ "$1" = "init-db" ]; then
 
-    chmod 0600 $agora_root/postgres/.pgpass
-    docker run -it --rm --net=host -v $agora_root/config/scan:/src -v $agora_root/postgres/.pgpass:/root/.pgpass postgres:12.0 psql -f /src/tables.sql -d db -h 0.0.0.0 -U agora
+    chmod 0600 "$agora_root"/postgres/.pgpass
+    docker run -it --rm --net=host -v "$agora_root"/config/scan:/src -v "$agora_root"/postgres/.pgpass:/root/.pgpass postgres:12.0 psql -f /src/tables.sql -d db -h 0.0.0.0 -U agora
 
 elif [ "$1" = "start-agora-scan" ]; then
 
-    docker-compose -f $agora_root/agora-scan/docker-compose.yml up -d
+    docker-compose -f "$agora_root"/agora-scan/docker-compose.yml up -d
 
 elif [ "$1" = "stop-agora-scan" ]; then
 
-    docker-compose -f $agora_root/agora-scan/docker-compose.yml down
+    docker-compose -f "$agora_root"/agora-scan/docker-compose.yml down
 
 else
 
