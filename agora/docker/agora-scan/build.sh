82e58a638e99c4309ad652a3cf27f74ef401cd75
michael <mukeunkim@gmail.com>
2023-05-12 17:36:32 +0900

    Automatically generate tag names for docker images

--- agora/docker/agora-scan/build.sh
+++ agora/docker/agora-scan/build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-docker buildx build --platform=linux/amd64,linux/arm64 -t bosagora/agora-scan:agora-v1.0.0 -f Dockerfile --push ../../../../agora-scan
+CURRENT_POS="$(pwd)"
+cd ../../../../agora-scan
+TAG_NANE="$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short=6 HEAD)"
+echo "TAG_NANE=$TAG_NANE"
+cd "$CURRENT_POS"
+docker buildx build --platform=linux/amd64,linux/arm64 -t bosagora/agora-scan:"$TAG_NANE" -f Dockerfile --push ../../../../agora-scan
