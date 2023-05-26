b13c0b1a1cb27c664e827e68630ff209e4513ae4
Michael Kim <michael.kim.200518@gmail.com>
2023-05-26 10:44:53 +0900

    Add table blocks_bls_change

--- agora/config/scan/migration-20230524.sql
+++ agora/config/scan/migration-20230524.sql
@@ -1,11 +1,11 @@
-
 alter table validator_balances_p
     add withdrawal bigint default 0 not null;
 
 alter table validator_balances_p
     add total_balance bigint default 0 not null;
 
-update validator_balances_p set total_balance = balance + withdrawal;
+update validator_balances_p
+set total_balance = balance + withdrawal;
 
 alter table validator_balances_recent
     add withdrawal bigint default 0 not null;
@@ -19,22 +19,39 @@ create index idx_validator_balances_recent_withdrawal
 create index idx_validator_balances_recent_total_balance
     on validator_balances_recent (total_balance);
 
-update validator_balances_recent set total_balance = balance + withdrawal;
+update validator_balances_recent
+set total_balance = balance + withdrawal;
 
-drop table if exists blocks_withdrawals;
-create table blocks_withdrawals
+
+CREATE TABLE IF NOT EXISTS
+    blocks_withdrawals
 (
-    block_slot          int    not null,
-    block_root          bytea  not null default '',
-    withdrawalindex     int    not null,
-    validatorindex      int    not null,
-    address             bytea  not null,
-    amount              bigint not null,
-    primary key (block_slot, block_root, withdrawalindex)
+    block_slot      INT    NOT NULL,
+    block_root      bytea  NOT NULL,
+    withdrawalindex INT    NOT NULL,
+    validatorindex  INT    NOT NULL,
+    address         bytea  NOT NULL,
+    amount          BIGINT NOT NULL,
+    -- in GWei
+    PRIMARY KEY (block_slot, block_root, withdrawalindex)
 );
-create index idx_blocks_withdrawals_slot on blocks_withdrawals (block_slot);
-create index idx_blocks_withdrawals_block_root on blocks_withdrawals (block_root);
-create index idx_blocks_withdrawals_withdrawalindex on blocks_withdrawals (withdrawalindex);
-create index idx_blocks_withdrawals_validatorindex on blocks_withdrawals (validatorindex);
-create index idx_blocks_withdrawals_address on blocks_withdrawals (address);
-create index idx_blocks_withdrawals_amount on blocks_withdrawals (amount);
+
+CREATE INDEX IF NOT EXISTS idx_blocks_withdrawals_recipient ON blocks_withdrawals (address);
+
+CREATE INDEX IF NOT EXISTS idx_blocks_withdrawals_validatorindex ON blocks_withdrawals (validatorindex);
+
+CREATE TABLE IF NOT EXISTS
+    blocks_bls_change
+(
+    block_slot     INT   NOT NULL,
+    block_root     bytea NOT NULL,
+    validatorindex INT   NOT NULL,
+    signature      bytea NOT NULL,
+    pubkey         bytea NOT NULL,
+    address        bytea NOT NULL,
+    PRIMARY KEY (block_slot, block_root, validatorindex)
+);
+
+CREATE INDEX IF NOT EXISTS idx_blocks_bls_change_pubkey ON blocks_bls_change (pubkey);
+
+CREATE INDEX IF NOT EXISTS idx_blocks_bls_change_address ON blocks_bls_change (address);
