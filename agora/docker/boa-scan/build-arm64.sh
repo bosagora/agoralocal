a5f492ca4350625258281aa62f5e0ad7ea0e52b6
HyeonyeobKim <kimhyunb@gmail.com>
2023-06-02 16:17:40 +0900

    Upgrade BOAscan v5.1.5

--- agora/docker/boa-scan/build-arm64.sh
+++ agora/docker/boa-scan/build-arm64.sh
@@ -5,7 +5,7 @@ cd ../../../../boa-scan
 TAG_NANE="$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short=6 HEAD)-arm64"
 echo "TAG_NANE=$TAG_NANE"
 cd "$CURRENT_POS"
-docker build -f Dockerfile --build-arg RELEASE_VERSION="5.1.4" -t bosagora/boa-scan:"$TAG_NANE" ../../../../boa-scan
-docker build -f Dockerfile --build-arg RELEASE_VERSION="5.1.4" -t bosagora/boa-scan:agora-arm64 ../../../../boa-scan
+docker build -f Dockerfile --build-arg RELEASE_VERSION="5.1.5" -t bosagora/boa-scan:"$TAG_NANE" ../../../../boa-scan
+docker build -f Dockerfile --build-arg RELEASE_VERSION="5.1.5" -t bosagora/boa-scan:agora-arm64 ../../../../boa-scan
 docker push bosagora/boa-scan:"$TAG_NANE"
 docker push bosagora/boa-scan:agora-arm64
