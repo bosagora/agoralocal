8f9773fb6ef6e2c53df29376660a8780bfbf681f
Michael Kim <michael.kim.200518@gmail.com>
2023-06-02 11:01:07 +0900

    Upgrade boa scan docker image

--- agora/docker/boa-scan/build-amd64.sh
+++ agora/docker/boa-scan/build-amd64.sh
@@ -6,3 +6,6 @@ TAG_NANE="$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short=6 HEAD)-amd
 echo "TAG_NANE=$TAG_NANE"
 cd "$CURRENT_POS"
 docker build -f Dockerfile --build-arg RELEASE_VERSION="5.1.4" -t bosagora/boa-scan:"$TAG_NANE" ../../../../boa-scan
+docker build -f Dockerfile --build-arg RELEASE_VERSION="5.1.4" -t bosagora/boa-scan:agora-amd64 ../../../../boa-scan
+docker push bosagora/boa-scan:"$TAG_NANE"
+docker push bosagora/boa-scan:agora-amd64
